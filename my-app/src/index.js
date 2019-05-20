dfaedfd91aa2a2d4b6531228d77ecabca06f8351
Guo <dguo@dgmbptycm15294.corp.microstrategy.com>
2019-05-20 17:12:27 -0400

    add winner logic

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,13 +16,31 @@ function Square(props) {
   winner: null
 }*/
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.initialState = {
       squares: Array(9).fill(null),
-      xIsNext: true,
-      winner: null
+      xIsNext: true
     };
     this.state = this.initialState;
   }
@@ -35,30 +53,25 @@ class Board extends React.Component {
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
   }
   
-  checkWinner(i) {
-    return null;
-  }
-  
   handleClick(i) {
-    if (this.state.squares[i] == null && this.state.winner == null) {
-      const squares = this.state.squares.slice();
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      let winner = this.checkWinner(i);
-
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext,
-        winner: winner
-      });
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
+    const winner = calculateWinner(this.state.squares);
     let status;
-    if (this.state.winner != null) {
-      status = "Winner: " + this.state.winner;
+    if (winner) {
+      status = 'Winner: ' + winner;
     } else {
-      status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
 
     return (
